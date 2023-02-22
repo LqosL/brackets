77d9b04968da8c2bd9551e8c1f8675ff8924d504
LqosL <camilla_agliullina@mail.ru>
2023-02-22 21:12:52 +0600

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let closing = [];
+    for (let i = 0; i < str.length; i++){
+        let index;
+        for (let j = 0; j < bracketsConfig.length; j ++) {
+            if (bracketsConfig[j][0] == str.charAt(i)){
+                index = j;
+                break;
+            }
+        }
+        switch (true) {
+            case str.charAt(i) == closing[closing.length-1]:
+                closing.pop()
+                break;
+            case index !== undefined:
+                closing.push(bracketsConfig[index][1]);
+                break;
+            default:
+                return false
+        }
+    }
+    return closing.length == 0
 }
